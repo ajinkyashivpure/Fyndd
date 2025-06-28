90c8666a6ade92a931897d33277695c8e61593f9
SatvikRaina <s7vikraina@gmail.com>
2025-06-29 02:11:37 +0530

    refresh problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import ExplorePage from './pages/ExplorePage.jsx';
 import CategoryPage from './pages/CategoryPage.jsx';
 import AestheticsPage from "./pages/AestheticsPage.jsx";
 import ProductsPage from './pages/ProductsPage.jsx';
 import SearchPage from './pages/SearchPage.jsx';
-
 
 function App() {
     return (
@@ -17,6 +16,8 @@ function App() {
                 <Route path="/aesthetics" element={<AestheticsPage />} />
                 <Route path="/products" element={<ProductsPage />} />
                 <Route path="/search" element={<SearchPage />} />
+                {/* Catch-all route - must be last */}
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </Router>
     );
