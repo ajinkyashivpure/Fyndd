1a478c5f6f2fe66a8896f4bcd4d84b8d1fe0cbb5
ajinkyashivpure <ajinkyashivpure@gmail.com>
2025-11-04 01:15:36 +0530

    sitemap in public

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { SitemapPlugin } from 'vite-plugin-sitemap'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss() ,SitemapPlugin({
-    hostname: 'https://fyndd.in',
-    routes: ['/', '/features', '/about'],
-  }),],
+  plugins: [react() , tailwindcss(),],
   server: {
     historyApiFallback: true,
   },
