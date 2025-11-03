5339112755b6e5eb034fa3962bc74ed6bd6bc712
ajinkyashivpure <ajinkyashivpure@gmail.com>
2025-11-04 01:03:04 +0530

    sitemap

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { SitemapPlugin } from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss() ,],
+  plugins: [react() , tailwindcss() ,SitemapPlugin({
+    hostname: 'https://fyndd.in',
+    routes: ['/', '/features', '/about'],
+  }),],
   server: {
     historyApiFallback: true,
   },
